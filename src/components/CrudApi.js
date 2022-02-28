95e857b5630495c84ffc16064d2bad06cee7c335
MauricioA03 <mauricio.crispink@gmail.com>
2022-02-28 17:16:10 -0400

    React-32-Crud Api- Creating a Api with JSON server (1/5)

---
+++ src/components/CrudApi.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+const CrudApi = () => {
+    const [db, setDb] = useState([]);
+    const [dataToEdit, setDataToEdit] = useState(null);
+
+    const createData = (data) => {
+        data.id = Date.now();
+        setDb([...db, data]);
+    };
+    const updateData = (data) => {
+        let newData = db.map((el) => el.id === data.id ? data : el);
+        setDb(newData);
+    };
+    const deleteData = (id) => {
+        let idDelete = window.confirm(`Estas seguro de eliminar el regristo con el ID '${id}'`);
+        if (idDelete) {
+            let newData = db.filter(el => el.id !== id);
+            setDb(newData);
+        } else {
+            return;
+        }
+    };
+    return (
+        <div>
+            <h2>CRUD App</h2>
+            <article className={"grid-1-2"}>
+                <CrudForm createData={createData}
+                          updateData={updateData}
+                          dataToEdit={dataToEdit}
+                          setDataToEdit={setDataToEdit}
+                />
+                <CrudTable data={db}
+                           setDataToEdit={setDataToEdit}
+                           deleteData={deleteData}
+                />
+            </article>
+        </div>
+    );
+};
+
+export default CrudApi;

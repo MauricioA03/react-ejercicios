c1674769b0951e67b56f6b76af3639cf8b839831
MauricioA03 <mauricio.crispink@gmail.com>
2022-02-28 18:52:26 -0400

    React-35-Crud Api- Implementing Loaders and Error messages(4/5)

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -2,34 +2,45 @@ import React, {useEffect, useState} from 'react';
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
 import {helpHttp} from "../helpers/helpHttp";
+import Loader from "./Loader";
+import Message from "./Message";
 
 const CrudApi = () => {
-    const [db, setDb] = useState([]);
+    const [db, setDb] = useState(null);
     const [dataToEdit, setDataToEdit] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     let api = helpHttp();
     let url = "http://localhost:5000/santos";
 
     useEffect(() => {
+        setLoading(true);
         api.get(url).then(res => {
             //console.log(res)
             if (!res.err) {
-                setDb(res)
+                setDb(res);
+                setError(null);
             } else {
                 setDb(null);
+                setError(res);
             }
+
+            setLoading(false);
         });
-    }, []);
+    }, [url]);
 
 
     const createData = (data) => {
         data.id = Date.now();
         setDb([...db, data]);
     };
+
     const updateData = (data) => {
         let newData = db.map((el) => el.id === data.id ? data : el);
         setDb(newData);
     };
+
     const deleteData = (id) => {
         let idDelete = window.confirm(`Estas seguro de eliminar el regristo con el ID '${id}'`);
         if (idDelete) {
@@ -39,19 +50,24 @@ const CrudApi = () => {
             return;
         }
     };
+
     return (
         <div>
             <h2>CRUD API</h2>
             <article className={"grid-1-2"}>
-                <CrudForm createData={createData}
-                          updateData={updateData}
-                          dataToEdit={dataToEdit}
-                          setDataToEdit={setDataToEdit}
+                <CrudForm
+                    createData={createData}
+                    updateData={updateData}
+                    dataToEdit={dataToEdit}
+                    setDataToEdit={setDataToEdit}
                 />
-                <CrudTable data={db}
-                           setDataToEdit={setDataToEdit}
-                           deleteData={deleteData}
-                />
+                {loading && <Loader/>}
+                {error && <Message msg={`Error ${error.status}:${error.statusText}`} bgColor="#dc3545"/>}
+                {db && <CrudTable
+                    data={db}
+                    setDataToEdit={setDataToEdit}
+                    deleteData={deleteData}
+                />}
             </article>
         </div>
     );

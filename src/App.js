2500f80f03d8bd09aa995ad4020a228880af2f4e
MauricioA03 <mauricio.crispink@gmail.com>
2022-03-01 16:26:35 -0400

    React-37-Song Search- Components and logic definition(1/5)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import CrudApp from "./components/CrudApi/CrudApp";
 import CrudApi from "./components/CrudApi/CrudApi";
+import SongSearch from "./components/SongSearch/SongSearch";
 
 function App() {
     return (
         <>
             <h1>Ejercicios con React</h1>
+            <SongSearch/>
+            <hr/>
             <CrudApi/>
             <hr/>
             <CrudApp/>

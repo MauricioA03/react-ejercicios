9290405463b4b887c09e424808e518785c52d892
MauricioA03 <mauricio.crispink@gmail.com>
2022-03-01 17:08:01 -0400

    React-39-Song Search- Ajax request to APIs(2/5)

--- src/components/SongSearch/SongSearch.js
+++ src/components/SongSearch/SongSearch.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import SongForm from "./SongForm";
 import SongDetails from "./SongDetails";
 import Loader from "../Loader";
+import {helpHttp} from "../../helpers/helpHttp";
 
 const SongSearch = () => {
 
@@ -10,10 +11,30 @@ const SongSearch = () => {
     const [bio, setBio] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    //repasar clase 17 
+    useEffect(() => {
+        if (search === null) return;
+        const fetchData = async () => {
+            const {artist, song} = search;
+            let artistUrl = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+            let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+            console.log(artistUrl, songUrl);
+            setLoading(true);
+            const [artistRes, songRes] = await Promise.all([helpHttp().get(artistUrl), helpHttp().get(songUrl),]);
+            console.log(artistRes, songRes);
+
+            setBio(artistRes);
+            setLyric(songRes);
+            setLoading(false);
+        };
+
+        fetchData();
+    }, [search]);
+
+
     const handleSearch = (data) => {
         setSearch(data);
     };
-
 
     return (
         <div>

9232c4670855c7696abee83d54c82e3f883b9d95
MauricioA03 <mauricio.crispink@gmail.com>
2022-03-02 17:12:42 -0400

    React-44-Selects Anidados- Data render and CSS styles(3/3)

--- src/components/SelectsAnidados/SelectsAnidados.js
+++ src/components/SelectsAnidados/SelectsAnidados.js
@@ -7,22 +7,34 @@ const SelectsAnidados = () => {
     const [town, setTown] = useState("");
     const [suburb, setSuburb] = useState("");
 
+    const TOKEN = "5325dd42-234d-48bc-9645-e2505791b5e3";
 
     return (
         <div>
             <h2>Selects anidados</h2>
             <h3>Mexico</h3>
-            <SelectList title={"estados"} url={""} handleChange={(e) => {
-                setState(e.target.value);
-            }}/>
+            <SelectList
+                title={"estado"}
+                url={`https://api.copomex.com/query/get_estados?token=${TOKEN}`}
+                handleChange={(e) => {
+                    setState(e.target.value);
+                }}/>
 
-            {state && <SelectList title={"municipios"} url={""} handleChange={(e) => {
-                setTown(e.target.value);
-            }}/>}
+            {state &&
+                <SelectList
+                    title={"municipios"}
+                    url={`https://api.copomex.com/query/get_municipio_por_estado/${state}?token=${TOKEN}`}
+                    handleChange={(e) => {
+                        setTown(e.target.value);
+                    }}/>}
 
-            {town && <SelectList title={"colonias"} url={""} handleChange={(e) => {
-                setSuburb(e.target.value);
-            }}/>}
+            {town &&
+                <SelectList
+                    title={"colonia"}
+                    url={`https://api.copomex.com/query/get_colonia_por_municipio/${town}?token=${TOKEN}`}
+                    handleChange={(e) => {
+                        setSuburb(e.target.value);
+                    }}/>}
 
             <pre>
                 <code>

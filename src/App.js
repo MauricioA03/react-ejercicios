b1a508c2bf988c16734bed6977a5309f6ac26f38
MauricioA03 <mauricio.crispink@gmail.com>
2022-03-02 20:31:12 -0400

    React-45-Form Validation- Components definition and logic (1/4)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import CrudApp from "./components/CrudApi/CrudApp";
 import CrudApi from "./components/CrudApi/CrudApi";
 import SongSearch from "./components/SongSearch/SongSearch";
 import SelectsAnidados from "./components/SelectsAnidados/SelectsAnidados";
+import ContacForm from "./components/FormValidation/ContacForm";
 
 function App() {
     return (
         <>
             <h1>Ejercicios con React</h1>
+            <ContacForm/>
+            <hr/>
             <SelectsAnidados/>
             <hr/>
             <SongSearch/>
